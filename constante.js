752ad22a4265be55b0bfc4fe5eeafa24803f8a2b
Gonzalo Avila <gavilavenegas4@gmail.com>
2025-08-27 11:36:20 -0400

    Modulos

---
+++ constante.js
@@ -0,0 +1,14 @@
+let estudiantes = ["Jose","Gonzalo","Camila"]
+
+
+
+function suma(){
+    return 2+2
+}
+
+function resta(){
+    return 2-2
+}
+
+//common JS
+module.exports = {estudiantes,suma,resta}
---
+++ main.js
@@ -0,0 +1,9 @@
+//utilizar modulos  
+
+const x = require('./constante.js')
+
+
+console.log(x.estudiantes)
+console.log(x.suma())
+console.log(x.resta())
+ 
